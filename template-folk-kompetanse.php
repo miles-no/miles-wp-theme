67539694ff63d367d30de23c50071e8e6648e7bd
Knut Hufthammer <knut.hufthammer@miles.no>
2023-10-26 09:40:36 +0200

    bugfix: vertical align 'Alle' filter button

--- template-folk-kompetanse.php
+++ template-folk-kompetanse.php
@@ -118,8 +118,9 @@ function consultant_as_webcomponent( $consultant, $hidden ): array {
         <div class="miles_offices_bar cv-filter-tags" aria-description="Våre kontor i Norge." aria-label="Kontor">
             <span>Kontor</span>
             <ul>
-            <miles-filter-button filter="" color="#3F1221"
-                                             href="<?php echo $current_url; ?>">Alle</miles-filter-button>
+                <li>
+                    <miles-filter-button filter="" color="#3F1221" href="<?php echo $current_url; ?>">Alle</miles-filter-button>
+                </li>
                 <?php foreach ($offices as $office): ?>
                     <li <?php echo $office["class"] ?>>
                         <miles-filter-button filter="<?php echo $office['officeId']; ?>" <?php echo $office["selected"] ? 'selected' : '' ?> color="#3F1221"
@@ -131,8 +132,9 @@ function consultant_as_webcomponent( $consultant, $hidden ): array {
         <div class="miles_areas_bar cv-filter-tags" aria-description="Hva vi er gode på." aria-label="Fagområder">
             <span>Fagområder</span>
             <ul>
-            <miles-filter-button filter="" color="#3F1221"
-                                             href="<?php echo $current_url; ?>">Alle</miles-filter-button>
+                <li>
+                    <miles-filter-button filter="" color="#3F1221" href="<?php echo $current_url; ?>">Alle</miles-filter-button>
+                </li>
                 <?php foreach ($roles as $role): ?>
                     <li <?php echo $role["class"] ?>>
                     <miles-filter-button filter="<?php echo $role['roleId']; ?>" <?php echo $role["selected"] ? 'selected' : '' ?> color="#3F1221"
